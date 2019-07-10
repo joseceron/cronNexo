4ebbc17a7648a6e44a8666fbc55a5b64d6af8f40
joseceron <joseluisceron13@gmail.com>
2019-07-10 17:06:44 -0500

    just crons

--- app.js
+++ app.js
@@ -1,78 +1,78 @@
-const express = require('express')
-const ngrok = require('ngrok');
+// const express = require('express')
+// const ngrok = require('ngrok');
 
-const relevo = require('./utils/relevo.js')
+// const relevo = require('./utils/relevo.js')
 
 var cleanSercorisac = require('./utils/cleanSercorisac.js')
 var lavisaSQL = require('./utils/lavisaSQL.js')
 var Scheduled = require("scheduled");
-var bodyParser     =        require("body-parser");
+// var bodyParser     =        require("body-parser");
 
-const app = express()
-const port = process.env.PORT || 3000
-// server configuration
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// const app = express()
+// const port = process.env.PORT || 3000
+// // server configuration
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
-var chalk = require('chalk')
+// var chalk = require('chalk')
 
 
-app.get('', (req, res) => {
-    return res.send({
-        mensaje: 'default'
-    })
-})
+// app.get('', (req, res) => {
+//     return res.send({
+//         mensaje: 'default'
+//     })
+// })
 
-app.get('/startcronsercorisac', (req, res) => {
-    return res.send({
-        mensaje: 'index'
-    })
-})
+// app.get('/startcronsercorisac', (req, res) => {
+//     return res.send({
+//         mensaje: 'index'
+//     })
+// })
 
-app.post('/pedido',(req,res) =>{
+// app.post('/pedido',(req,res) =>{
     
-    // let idRelevo = req.query.idRelevo;
-    // let idVisita = req.query.idVisita;   
-    // console.log(req.query) 
-    let idRelevo = req.body.idRelevo;
-    let idVisita = req.body.idVisita;   
-    // console.log(req.query)
-    console.log(req.body) 
+//     // let idRelevo = req.query.idRelevo;
+//     // let idVisita = req.query.idVisita;   
+//     // console.log(req.query) 
+//     let idRelevo = req.body.idRelevo;
+//     let idVisita = req.body.idVisita;   
+//     // console.log(req.query)
+//     console.log(req.body) 
         
-    // console.log(req)
-    return mensaje(idVisita,idRelevo,res)
+//     // console.log(req)
+//     return mensaje(idVisita,idRelevo,res)
     
-})
+// })
 
 
-async function mensaje(idVisita, idRelevo,res){
-    me =  await relevo.getRelevo(idVisita, idRelevo)        
-   return res.send({
-    success: me
-   })
+// async function mensaje(idVisita, idRelevo,res){
+//     me =  await relevo.getRelevo(idVisita, idRelevo)        
+//    return res.send({
+//     success: me
+//    })
     
-}
+// }
 
 
 
-// var myJob = new Scheduled({
-//     id: "Lavisa",
-//     pattern: "*/1 14-23 * * 1-5", // Tarea a ejecutar cada dos minutos
-//     task: function(){
-//         console.log("Job Lavisa distribuidora");
-//         lavisaSQL.ajustarPedidos()
-//     }
-// }).start();
+var myJob = new Scheduled({
+    id: "Lavisa",
+    pattern: "*/1 14-23 * * 1-5", // Tarea a ejecutar cada dos minutos
+    task: function(){
+        console.log("Job Lavisa distribuidora");
+        lavisaSQL.ajustarPedidos()
+    }
+}).start();
 
  
-// var jobSercorisac = new Scheduled({
-//     id: "Sercorisac",
-//     pattern: "*/1 14-23 * * 1-5", // Tarea a ejecutar cada dos minutos
-//     task: function(){
-//         console.log("Job Sercorisacn en integrado");
-//         cleanSercorisac.iniciar()
-//     }
-// }).start();
+var jobSercorisac = new Scheduled({
+    id: "Sercorisac",
+    pattern: "*/1 14-23 * * 1-5", // Tarea a ejecutar cada dos minutos
+    task: function(){
+        console.log("Job Sercorisacn en integrado");
+        cleanSercorisac.iniciar()
+    }
+}).start();
 
 
 // This metod needs to be last
@@ -85,9 +85,9 @@ async function mensaje(idVisita, idRelevo,res){
 // })
 
 
-app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
+// app.listen(port, () => {
+//     console.log('Server is up on port ' + port)
+// })
 
 // ngrok.connect({
 //     proto: 'http',
